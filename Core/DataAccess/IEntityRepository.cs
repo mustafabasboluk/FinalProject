81e0e59f8f9bbbcf3f05d17ebe71c6e08c65144d
Mustafa <mustafabasboluk@hotmail.com>
2021-02-06 22:05:04 +0300

    6 Şubat mola 1

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,16 +1,16 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint  --  generic kisit
     //class : referans tip
     //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
     //new() : new'lenebilir olmali
+    //Core katmani diger katmanlardan referans almaz
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
