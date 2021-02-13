73386cdcec57d863659305aa99223027f2b024ac
Mustafa <mustafabasboluk@hotmail.com>
2021-02-13 22:47:49 +0300

    13 Şubat 1.Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,10 +35,10 @@ namespace Business.Concrete
         {
             //Is Kodlari
             //Yetkisi var mi?
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintanceTime);
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
